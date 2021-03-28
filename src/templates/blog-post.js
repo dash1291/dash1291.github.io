8092863e1fb1f516a9c90b86d0ee3e441c155752
Ashish Dubey <ashish.dubey91@gmail.com>
2021-03-28 13:15:56 +0530

    remove disqus

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -37,7 +37,6 @@ class BlogPostTemplate extends React.Component {
             marginBottom: rhythm(1),
           }}
         />
-        <DiscussionEmbed shortname={disqusShortname} config={disqusConfig} />
 
         <ul
           style={{
